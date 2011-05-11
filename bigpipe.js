ed8e76395486fc1801c72f9e858301b1488224e8
Rafael Carício <rafael.jacinto@gmail.com>
2011-05-11 16:58:28 -0300

    Initial setup.

--- bigpipe.js
+++ bigpipe.js
@@ -1,3 +1,14 @@
+/*
+*
+* Bigpipe JS
+* https://github.com/adonescunha/bigpipe
+*
+* Licensed under the MIT license:
+* http://www.opensource.org/licenses/mit-license
+* Copyright (c) 2011 Adones Cunha adones@atepassar.com
+*
+*/
+
 var PageletResource = function(name, pagelet) {
   var that = {};
   that.name = name;
