bbe1b026134a88f046cfbbb86233a857eaa6d45e
Rafael Carício <rafael.jacinto@gmail.com>
2011-05-12 14:45:06 -0300

    Updated copyrights.

--- src/cssResource.js
+++ src/cssResource.js
@@ -5,7 +5,7 @@
 *
 * Licensed under the MIT license:
 * http://www.opensource.org/licenses/mit-license
-* Copyright (c) 2011 Adones Cunha adones@atepassar.com
+* Copyright (c) 2011 Orygens contato@orygens.com
 *
 */
 
