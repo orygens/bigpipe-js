72556acf9a333a3995bd4c3f4d90025eafdd56ac
Rafael Carício <rafael.jacinto@gmail.com>
2011-05-12 01:08:20 -0300

    Fix.

---
+++ src/bigpipe.js
@@ -0,0 +1,29 @@
+/*
+*
+* Bigpipe JS
+* https://github.com/adonescunha/bigpipe
+*
+* Licensed under the MIT license:
+* http://www.opensource.org/licenses/mit-license
+* Copyright (c) 2011 Adones Cunha adones@atepassar.com
+*
+*/
+
+(function($) {
+
+    $.widget("bigpipe.bigpipe", {
+
+        options: {
+            pagelets: []
+        },
+
+        _create: function() {
+            this._phase = 0;
+        },
+
+        pagelet: function(data) {
+        }
+
+    });
+
+})(jQuery);
