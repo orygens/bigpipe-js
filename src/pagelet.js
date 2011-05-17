0f34613e450228065d17cf6a717d2e1d855a40fe
Rafael Carício <rafael.jacinto@gmail.com>
2011-05-17 01:28:17 -0300

    Pagelet plugin implemented but not yet tested.

--- src/pagelet.js
+++ src/pagelet.js
@@ -13,9 +13,50 @@
 
     $.widget("bigpipe.Pagelet", {
 
-        options: {},
+        options: {
+            id: "",
+            content: null,
+            url: null,
+            css: [],
+            js: []
+        },
 
         _create: function() {
+            this.element.bind("loadCSS.BigPipe", $.proxy(this._loadCSS, this));
+            this.element.bind("loadContent.BigPipe", $.proxy(this._loadContent, this));
+            this.element.bind("loadJS.BigPipe", $.proxy(this._loadJS, this));
+        },
+
+        _loadCSS: function() {
+            $.each(this.options.css, $.proxy(function(cssFile) {
+                this.element.trigger("loadCSSResource", cssFile);
+            }, this));
+
+            this.element.trigger("cssLoaded");
+        },
+
+        _loadContent: function() {
+            var loadContent = function(content) {
+                this.element.find("#" + this.options.id).html(content);
+                this.element.trigger("contentLoaded");
+            }
+
+            if (this.options.content) {
+                loadContent(this.options.content);
+            } else if (this.options.url) {
+                $.ajax({
+                    url: this.options.url,
+                    success: $.proxy(loadContent, this)
+                });
+            }
+        },
+
+        _loadJS: function() {
+            $.each(this.options.js, $.proxy(function(jsFile) {
+                this.element.trigger("loadJSResource", jsFile);
+            }, this));
+
+            this.element.trigger("jsLoaded");
         }
 
     });
