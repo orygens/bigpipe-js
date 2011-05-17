890d2d9f7be9eff452a1468f2a217e1bbd878a69
Bernardo Heynemann <heynemann@gmail.com>
2011-05-17 13:40:03 -0300

    Improved usage

--- ref/usage.js
+++ ref/usage.js
@@ -1,16 +1,32 @@
-$(".content").bigpipe({
-    pagelets: [
-        {
-            id: "timeline",
-            content: "/home#!pagelet=timeline", 
-            css: ["/media/common.css", "/media/css/timeline.css"],
-            js: ["/media/js/timeline.js"]
-        },
-        {
-            id: "friends_box",
-            content: "/home#!pagelet=friends_box", 
-            css: ["/media/css/friends_box.css"],
-            js: []
-        }
-    ]
-});
+var pagelets = [
+    {
+        container: $('div'),
+        url: "/home#!pagelet=timeline", 
+        css: ["/media/common.css", "/media/css/timeline.css"],
+        js: ["a.js", "b.js", "c.js"]
+    },
+    {
+        container: "friends_box",
+        content: "/home#!pagelet=friends_box", 
+        css: ["/media/css/friends_box.css"],
+        js: ["a.js", "d.js", "e.js"]
+    },
+    {
+        container: "friends_box",
+        content: "/home#!pagelet=friends_box", 
+        css: ["/media/css/friends_box.css"],
+        js: ["a.js", "j.js"]
+    }
+
+];
+
+$(".content").bigpipe(pagelets);
+
+a - 3 + 3 = 6
+j - 1
+b - 2
+c - 1
+d - 2
+e - 1
+
+a, [b,d], [c,e,j]
